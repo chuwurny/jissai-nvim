4554d393e4eb5149d2e0e820b165c4761b8214ad
chuwurny <chuwurny@national.shitposting.agency>
2026-07-09 04:02:57 +0300

    feat: make flash-nvim search more visible

--- lua/jissai/groups/by-plugins/flash-nvim.lua
+++ lua/jissai/groups/by-plugins/flash-nvim.lua
@@ -2,4 +2,7 @@
 ---@param gr jissai.groups
 return function(hls, gr)
     gr.FlashBackdrop = hls.gui.notext
+    gr.FlashMatch = hls.macro
+    gr.FlashCurrent = { link = "FlashMatch" }
+    gr.FlashLabel = hls.gui.search_result
 end
