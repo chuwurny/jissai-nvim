e825f1f6a17e697aa8800dbcf569142da72dffa1
chuwurny <chuwurny@national.shitposting.agency>
2026-07-16 17:43:35 +0300

    feat: snacks support
    currently dim only

---
+++ lua/jissai/groups/by-plugins/snacks-nvim.lua
@@ -0,0 +1,9 @@
+---@diagnostic disable-next-line: unused
+---@param hls jissai.highlights
+---@param gr jissai.groups
+return function(hls, gr)
+    ---------
+    -- Dim --
+    ---------
+    gr.SnacksDim = hls.gui.notext
+end
