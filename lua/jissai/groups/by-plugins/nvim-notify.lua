76da71fce32eed527591246b07bb5db877da567d
chuwurny <chuwurny@national.shitposting.agency>
2026-07-16 04:57:53 +0300

    feat: suppress nvim-notify warning about `NotifyBackground`

---
+++ lua/jissai/groups/by-plugins/nvim-notify.lua
@@ -0,0 +1,6 @@
+---@diagnostic disable-next-line: unused
+---@param hls jissai.highlights
+---@param gr jissai.groups
+return function(hls, gr)
+    gr.NotifyBackground = hls.gui.background.focus
+end
